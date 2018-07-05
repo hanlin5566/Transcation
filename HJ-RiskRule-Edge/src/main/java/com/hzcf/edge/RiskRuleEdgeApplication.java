5ae4356f413dda960f25a87c346ca3e4fbe099dc
huhl <huhanlin@huizhongcf.com>
2018-07-05 16:34:53 +0800

    添加优雅停机

--- HJ-RiskRule-Edge/src/main/java/com/hzcf/edge/RiskRuleEdgeApplication.java
+++ HJ-RiskRule-Edge/src/main/java/com/hzcf/edge/RiskRuleEdgeApplication.java
@@ -3,15 +3,24 @@ package com.hzcf.edge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.web.client.RestTemplate;
+
+import com.hzcf.edge.components.redis.RedisClient;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.hzcf" })
 public class RiskRuleEdgeApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RiskRuleEdgeApplication.class, args);
+		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(RiskRuleEdgeApplication.class, args);
+        configurableApplicationContext.addApplicationListener(new ApplicationEventListener(configurableApplicationContext));
+
 	}
 	
 	/**
@@ -24,4 +33,27 @@ public class RiskRuleEdgeApplication {
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+}
+
+class ApplicationEventListener implements ApplicationListener {
+    
+	ConfigurableApplicationContext configurableApplicationContext;
+	
+    public ApplicationEventListener() {
+		super();
+	}
+    public ApplicationEventListener(ConfigurableApplicationContext configurableApplicationContext) {
+    	super();
+    	this.configurableApplicationContext = configurableApplicationContext;
+    }
+
+	@Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        // 在这里可以监听到Spring Boot的生命周期
+        if (event instanceof ContextStoppedEvent) { // 应用停止
+        } else if (event instanceof ContextClosedEvent) { // 应用关闭
+    		RedisClient redis = configurableApplicationContext.getBean(RedisClient.class);
+        	redis.destroy();
+        }
+    }
 }
